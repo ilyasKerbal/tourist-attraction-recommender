840bf6760be54d4942a93515e6894dff39504a35
Chanon Jenakom <chanonjenakom@gmail.com>
2017-10-09 23:57:37 +0700

    Add knowledge-base

---
+++ regions.pl
@@ -0,0 +1,13 @@
+:- module(regions,[]).
+
+region_id(1).
+region_id(2).
+region_id(3).
+region_id(4).
+region_id(5).
+
+region_name(1,north).
+region_name(2,north_east).
+region_name(3,central).
+region_name(4,east).
+region_name(5,south).
