a448260d1c883f5cc0553f4bdcf4bf98a4fc14a2
Chanon Jenakom <chanonjenakom@gmail.com>
2017-10-10 22:45:18 +0700

    Finish second query

--- engine.pl
+++ engine.pl
@@ -8,39 +8,119 @@
 
 thai_paradise :-
   writeln('Welcome to Thai Paradise!'),
-  thai_paradise_query.
+  thai_paradise_query('\nPlease select your action:').
 
-thai_paradise_query :-
-  writeln('\nPlease select your action:'),
-  writeln('\t1: Find tourist attractions based on activity'),
-  writeln('\t2: Find tourist attractions based on opening hours'),
-  writeln('\t3: Find tourist attractions based on age'),
-  writeln('\t4: Find tourist attractions based on cost'),
-  writeln('\t5: Find tourist attractions based on weather'),
-  writeln('\t0: Exit'),
+thai_paradise_query(Message) :-
+  writeln(Message),
+  writeln('1: Find tourist attractions based on activity'),
+  writeln('2: Find tourist attractions based on opening hours'),
+  writeln('3: Find tourist attractions based on age'),
+  writeln('4: Find tourist attractions based on cost'),
+  writeln('5: Find tourist attractions based on weather'),
+  writeln('0: Exit'),
   read(Choice),
   (Choice\=0 ->
     (Choice=1 ->
-      write('You have selected '),
-      writeln(Choice),
-      writeln('Please select an activity'),
+      writeln('\nPlease select an activity'),
       read(Activity),
       findall([TA,PR,RE,CA,RA],rules:suggest_from_activity(Activity,TA,PR,RE,CA,RA),List),
       length(List,Len),
       random(0,Len,Index),
       nth0(Index,List,[TouristAttraction,Province,Region,Category,Rating]),
-      writeln(''),
-      writeln('Result found!\n'),
+      writeln('\nResult found!\n'),
       format('Tourist Attraction: ~w\n',TouristAttraction),
       format('Province: ~w\n',Province),
       format('Region: ~w\n',Region),
       format('Category: ~w\n',Category),
-      format('Rating: ~w\n',Rating),
-      writeln('\nQuery finished!')
-    ;
-      writeln('Not a valid action!')
+      format('Rating: ~w\n',Rating)
+    ; Choice=2 ->
+      writeln('\nPlease select from:'),
+      writeln('1: Time of day'),
+      writeln('2: Opening and closing time'),
+      writeln('3: Opening time'),
+      writeln('4: Closing time'),
+      writeln('5: Currently opened'),
+      read(InnerChoice),
+      (InnerChoice=1 ->
+        writeln('Please select time of day'),
+        read(Time),
+        findall([TA,PR,RE,CA,RA,OT,CT],rules:suggest_from_opening_hours(Time,TA,PR,RE,CA,RA,OT,CT),List),
+        length(List,Len),
+        random(0,Len,Index),
+        nth0(Index,List,[TouristAttraction,Province,Region,Category,Rating,OpeningTime,ClosingTime]),
+        writeln('\nResult found!\n'),
+        format('Tourist Attraction: ~w\n',TouristAttraction),
+        format('Province: ~w\n',Province),
+        format('Region: ~w\n',Region),
+        format('Category: ~w\n',Category),
+        format('Rating: ~w\n',Rating),
+        format('Opening Time: ~w\n',OpeningTime),
+        format('Closing Time: ~w\n',ClosingTime)
+      ; InnerChoice=2 ->
+        writeln('Please select opening time'),
+        read(Opening),
+        writeln('Please select closing time'),
+        read(Closing),
+        findall([TA,PR,RE,CA,RA,OT,CT],rules:suggest_from_opening_and_closing_time(Opening,Closing,TA,PR,RE,CA,RA,OT,CT),List),
+        length(List,Len),
+        random(0,Len,Index),
+        nth0(Index,List,[TouristAttraction,Province,Region,Category,Rating,OpeningTime,ClosingTime]),
+        writeln('\nResult found!\n'),
+        format('Tourist Attraction: ~w\n',TouristAttraction),
+        format('Province: ~w\n',Province),
+        format('Region: ~w\n',Region),
+        format('Category: ~w\n',Category),
+        format('Rating: ~w\n',Rating),
+        format('Opening Time: ~w\n',OpeningTime),
+        format('Closing Time: ~w\n',ClosingTime)
+      ; InnerChoice=3 ->
+        writeln('Please select opening time'),
+        read(Opening),
+        findall([TA,PR,RE,CA,RA,OT,CT],rules:suggest_from_opening_time(Opening,TA,PR,RE,CA,RA,OT,CT),List),
+        length(List,Len),
+        random(0,Len,Index),
+        nth0(Index,List,[TouristAttraction,Province,Region,Category,Rating,OpeningTime,ClosingTime]),
+        writeln('\nResult found!\n'),
+        format('Tourist Attraction: ~w\n',TouristAttraction),
+        format('Province: ~w\n',Province),
+        format('Region: ~w\n',Region),
+        format('Category: ~w\n',Category),
+        format('Rating: ~w\n',Rating),
+        format('Opening Time: ~w\n',OpeningTime),
+        format('Closing Time: ~w\n',ClosingTime)
+      ; InnerChoice=4 ->
+        writeln('Please select closing time'),
+        read(Closing),
+        findall([TA,PR,RE,CA,RA,OT,CT],rules:suggest_from_closing_time(Closing,TA,PR,RE,CA,RA,OT,CT),List),
+        length(List,Len),
+        random(0,Len,Index),
+        nth0(Index,List,[TouristAttraction,Province,Region,Category,Rating,OpeningTime,ClosingTime]),
+        writeln('\nResult found!\n'),
+        format('Tourist Attraction: ~w\n',TouristAttraction),
+        format('Province: ~w\n',Province),
+        format('Region: ~w\n',Region),
+        format('Category: ~w\n',Category),
+        format('Rating: ~w\n',Rating),
+        format('Opening Time: ~w\n',OpeningTime),
+        format('Closing Time: ~w\n',ClosingTime)
+      ; InnerChoice=5 ->
+        writeln('Calculating current time'),
+        findall([TA,PR,RE,CA,RA,OT,CT],rules:suggest_from_opening_now(TA,PR,RE,CA,RA,OT,CT),List),
+        length(List,Len),
+        random(0,Len,Index),
+        nth0(Index,List,[TouristAttraction,Province,Region,Category,Rating,OpeningTime,ClosingTime]),
+        writeln('\nResult found!\n'),
+        format('Tourist Attraction: ~w\n',TouristAttraction),
+        format('Province: ~w\n',Province),
+        format('Region: ~w\n',Region),
+        format('Category: ~w\n',Category),
+        format('Rating: ~w\n',Rating),
+        format('Opening Time: ~w\n',OpeningTime),
+        format('Closing Time: ~w\n',ClosingTime)
+      )
     ),
-    thai_paradise_query
+    writeln('\nQuery finished!'),
+    thai_paradise_query('\nPlease select your next action:')
   ;
     write('Thank you for using Thai Paradise!')
   ).
